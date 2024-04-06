8d309e86548de33b8ca34edf1c0bf368c60c13bf
Mohammad Afzal <mohammadafzal@Mohammads-Mac-mini.local>
2024-04-06 15:28:14 -0400

    added custom domain name for frontendstack

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -1,4 +1,6 @@
 import { StackContext, StaticSite, use } from "sst/constructs";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+
 import { ApiStack } from "./ApiStack";
 
 export function FrontendStack({ stack, app }: StackContext) {
@@ -15,6 +17,17 @@ export function FrontendStack({ stack, app }: StackContext) {
     errorPage: "redirect_to_index_page",
     dev: {
       url: "http://localhost:5173",
+    },
+    customDomain: {
+      isExternalDomain: true,
+      domainName: "comedycellar.mafz.al",
+      cdk: {
+        certificate: Certificate.fromCertificateArn(
+          stack,
+          "FrontendCert",
+          "arn:aws:acm:us-east-1:824714483059:certificate/b75a7d49-f69b-405a-88d0-20f45c64d931"
+        ),
+      },
     },
   });
 
