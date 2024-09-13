122ccf5abaf24fb7429d213cb1dce43f2e8a4b1e
Mohammad Afzal <mohammadafzal@Mohammads-Mac-mini.local>
2024-09-13 00:18:15 -0400

    fixed request headers

--- packages/core/src/requester.ts
+++ packages/core/src/requester.ts
@@ -9,6 +9,9 @@ const axiosInstance = axios.create({
   headers: {
     "content-type": "application/json",
     "User-Agent": new UserAgent().toString(),
+    "x-code-localize":
+      "c52b87a6afa393757ae0b3d7b567123cffbaab341dc28426277eacc7c97c8b4e.MTcyNjE4ODAzNy03MS4xODMuMjQ5LjExMQ==",
+    "x-page-creation": +new Date(),
   },
 });
 
