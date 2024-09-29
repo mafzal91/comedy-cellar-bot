fcf2165aa21b9ff49bf607d2f2e873b3957e0f9f
Mohammad Afzal <mohammadafzal@Mohammads-Mac-mini.local>
2024-09-28 23:16:26 -0400

    Added Show and room db schemas

--- packages/core/common/constants.ts
+++ packages/core/common/constants.ts
@@ -1,2 +1,4 @@
 export const USER_PREFIX = "user";
 export const COMIC_PREFIX = "comic";
+export const SHOW_PREFIX = "show";
+export const ROOM_PREFIX = "room";
---
+++ packages/core/sql/room.sql.ts
@@ -0,0 +1,22 @@
+import { text, serial, varchar, timestamp, pgTable } from "drizzle-orm/pg-core";
+import { createExternalId } from "../common/createExternalId";
+import { ROOM_PREFIX } from "../common/constants";
+import { relations } from "drizzle-orm";
+import { show } from "./show.sql";
+
+export const room = pgTable("room", {
+  id: serial("id").notNull().primaryKey(),
+  externalId: varchar("externalId", { length: 128 })
+    .$defaultFn(() => createExternalId(ROOM_PREFIX))
+    .notNull()
+    .unique(),
+  name: text("name").notNull().unique(),
+  createdAt: timestamp("createdAt").defaultNow(),
+});
+
+export const usersRelations = relations(room, ({ many }) => ({
+  shows: many(show),
+}));
+
+export type SelectRoom = typeof room.$inferSelect;
+export type InsertRoom = typeof room.$inferInsert;
