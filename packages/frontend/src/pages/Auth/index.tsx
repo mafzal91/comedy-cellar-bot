1265e71e4b243bb0f4955ed9673a3fd27bcb8f2a
Mohammad Afzal <mafzal91@gmail.com>
2024-10-11 22:35:37 -0400

    ui update

--- packages/frontend/src/pages/Auth/index.tsx
+++ packages/frontend/src/pages/Auth/index.tsx
@@ -59,131 +59,112 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      {!user ? (
-        <form>
-          <h2>Sign Up</h2>
+    <form action="#" method="POST" className="space-y-6">
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email address
+        </label>
+        <div className="mt-2">
           <input
+            id="email"
+            name="email"
             type="email"
-            placeholder="Email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            required
+            autoComplete="email"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={signUp}>Sign Up</button>
-          {error && <p className="text-red-500">{error}</p>}
-        </form>
-      ) : (
-        <div>
-          <h2>Welcome, {username}</h2>
-          <button onClick={signOut}>Sign Out</button>
         </div>
-      )}
-    </div>
-  );
-};
-const SignIn = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [user, setUser] = useState(null);
+      </div>
 
-  // Create a user pool object with your UserPoolId and ClientId
-  const userPool = new CognitoUserPool({
-    UserPoolId: cognitoConfig.UserPoolId,
-    ClientId: cognitoConfig.ClientId,
-  });
-
-  const signIn = () => {
-    const authenticationData = {
-      Username: username,
-      Password: password,
-    };
-
-    const authenticationDetails = new AuthenticationDetails(authenticationData);
-
-    const userData = {
-      Username: username,
-      Pool: userPool,
-    };
-
-    const cognitoUser = new CognitoUser(userData);
-
-    cognitoUser.authenticateUser(authenticationDetails, {
-      onSuccess: (result) => {
-        const accessToken = result.getAccessToken().getJwtToken();
-        console.log("Access Token:", accessToken);
-        setUser(cognitoUser);
-      },
-      onFailure: (err) => {
-        setError(err.message || JSON.stringify(err));
-      },
-      newPasswordRequired: (userAttributes, requiredAttributes) => {
-        console.log("New password required");
-        // Handle new password scenario if required
-      },
-    });
-  };
-
-  const signOut = () => {
-    if (user) {
-      user.signOut();
-      setUser(null);
-      console.log("User signed out");
-    }
-  };
-
-  return (
-    <div>
-      {!user ? (
-        <form>
-          <h2>Login</h2>
+      <div>
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Password
+        </label>
+        <div className="mt-2">
           <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
+            id="password"
+            name="password"
             type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            required
+            autoComplete="current-password"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
-          <button onClick={signIn}>Log In</button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
-      ) : (
-        <div>
-          <h2>Welcome, {username}</h2>
-          <button onClick={signOut}>Sign Out</button>
         </div>
-      )}
-    </div>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <input
+            id="remember-me"
+            name="remember-me"
+            type="checkbox"
+            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+          />
+          <label
+            htmlFor="remember-me"
+            className="ml-3 block text-sm leading-6 text-gray-900"
+          >
+            Remember me
+          </label>
+        </div>
+        <div className="text-sm leading-6">
+          <a
+            href="#"
+            className="font-semibold text-indigo-600 hover:text-indigo-500"
+          >
+            Forgot password?
+          </a>
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
   );
 };
 
 function Auth() {
   const { route } = useLocation();
 
-  // useEffect(() => {
-  //   console.log(window.location.pathname);
-  //   if (window.location.pathname === "/auth") {
-  //     route(`/auth/sign-in`, true);
-  //   }
-  // }, []);
-
   return (
-    // <Router>
-    // <SignIn path="/auth/sign-in" />
-    <SignUp />
-    // <div default> Path not matched</div>
-    // </Router>
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img
+          alt="Your Company"
+          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+          className="mx-auto h-10 w-auto"
+        />
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <SignUp />
+        </div>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <a
+            href="#"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign Up here
+          </a>
+        </p>
+      </div>
+    </>
   );
 }
 
