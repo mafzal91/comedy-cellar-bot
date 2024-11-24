5e92e5f25d0cc2add5c71fb3389f73d1132bd250
Mohammad Afzal <mafzal91@gmail.com>
2024-11-24 11:31:59 -0500

    Only send email if there are shows

--- packages/functions/cron/newShowCron.ts
+++ packages/functions/cron/newShowCron.ts
@@ -1,11 +1,11 @@
 import { formatInTimeZone } from "date-fns-tz";
-import { getLastShow } from "@core/models/show";
-import { sendEmail } from "@core/email";
-import { parseTimestampString } from "@core/utils";
 import { getFutureDatesByDay } from "@core/getFutureDatesByDay";
-import { handleShowDetails } from "@core/handleShowDetails";
-import { sleep } from "@core/common/sleep";
+import { getLastShow } from "@core/models/show";
 import { handleLineUp } from "@core/handleLineUp";
+import { handleShowDetails } from "@core/handleShowDetails";
+import { parseTimestampString } from "@core/utils";
+import { sendEmail } from "@core/email";
+import { sleep } from "@core/common/sleep";
 
 const IS_ACTIVE = process.env.IS_ACTIVE === "1";
 const IS_CRON = process.env.IS_CRON === "1";
@@ -39,18 +39,20 @@ export async function handler() {
     const data = await handleShowDetails({ date: nextDayToFetch });
     console.log(dateForLogging, "Cron Data", data);
 
-    await sendEmail({
-      subject: "New Show Cron",
-      message: JSON.stringify(
-        {
-          executionTime: dateForLogging,
-          currentHour,
-          ...data,
-        },
-        null,
-        2
-      ),
-    }).catch((e) => console.error(e));
+    if (data.shows.length) {
+      await sendEmail({
+        subject: "New Show Cron",
+        message: JSON.stringify(
+          {
+            executionTime: dateForLogging,
+            currentHour,
+            ...data,
+          },
+          null,
+          2
+        ),
+      }).catch((e) => console.error(e));
+    }
 
     if (!data.shows.length) {
       moreShows = false;
